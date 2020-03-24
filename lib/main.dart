d768196c0a77ba13de35a0f555274190e7f51741
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2020-03-24 15:14:51 -0300

    atualizando

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart'; //interface para android
 import 'package:projetonovo/models/item.dart'; 
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
 void main() => runApp(App());
 
@@ -23,18 +25,25 @@ class MyHomePage extends StatefulWidget {
   var items = new List<Item>();
   MyHomePage(){
     items = [];
-    items.add(Item(title: "item 1", done: false));
-    items.add(Item(title: "item 2", done: true));
-    items.add(Item(title: "item 2", done: true));
-    items.add(Item(title: "item 2", done: true));
+   // items.add(Item(title: "item 1", done: false));
+   // items.add(Item(title: "item 2", done: true));
+   // items.add(Item(title: "item 2", done: true));
+   // items.add(Item(title: "item 2", done: true));
   }
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+
 class _MyHomePageState extends State<MyHomePage> {
   var newTaskCtrl = TextEditingController();
+
+_MyHomePageState() // inicializando uma vez só no construtor o load
+{
+  load();
+}
+
 
 void add(){  // função que adiciona novo objeto a lista
   if(newTaskCtrl.text.isEmpty) return;
@@ -46,6 +55,7 @@ void add(){  // função que adiciona novo objeto a lista
         done: false),
     );
     newTaskCtrl.text = "";
+    save();
   });
 }
 
@@ -53,9 +63,31 @@ void remove(int index)
 {
   setState(() { //removendo o indice marcado
     widget.items.removeAt(index);
+    save();
   });
 }
 
+
+//assíncrona (fila de atendimento)
+  Future load() async
+  {
+    var prefs = await SharedPreferences.getInstance(); // aguarde ate o sharedpreferences não estiver carregado
+    var data = prefs.getString('data');
+
+    if (data != null){ //deixando passar em branco
+      Iterable decoded = jsonDecode(data); // convertendo para json
+      List<Item> result = decoded.map((x) => Item.fromJson(x)).toList(); //percorrendo a lista e trazendo el
+      setState(() {
+        widget.items = result;
+      });
+    }
+  }
+
+//assíncrona
+save () async {
+  var prefs = await SharedPreferences.getInstance(); // pegando instancia do prefs
+  await prefs.setString('data', jsonEncode(widget.items)); //setando e transformando em uma lista
+}
 
   @override
   Widget build(BuildContext context) { //renderizo acabo n fica em um for... pra atualizar
@@ -93,6 +125,7 @@ void remove(int index)
             onChanged: (value){
               setState(() {
                 item.done = value; // mudando o checkbox
+                save();
               });
             },
           ),
